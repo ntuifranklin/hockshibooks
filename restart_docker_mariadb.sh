d2ba13991ead03b552356483603bf8e9c741061e
Alkebulan <alke@backup.stage.hockshi.com>
2024-11-25 19:17:20 +0000

    updated shell script

--- restart_docker_mariadb.sh
+++ restart_docker_mariadb.sh
@@ -1,4 +1,4 @@
 sudo docker stop mariadb && sudo docker rm mariadb && sudo docker run -p 3306:3306 -d --name mariadb \
  -e MYSQL_DATABASE=hockshi \
- -e MARIADB_ROOT_PASSWORD=Z+x3xxFqB0CDWsQqLSc6SghXDzKVzBa9yUuKlQZ9OeA= mariadb
+ -e MARIADB_ROOT_PASSWORD=SEE_ENV_SECRET mariadb
 mysql -h 172.17.0.2 -P 3306 -u root -p < database/sql/hockshidb.sql
